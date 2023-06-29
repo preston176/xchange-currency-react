47a4ff73a86ab56b511be655281b33c5c4670c81
preston176 <prestonnyamweya@gmail.com>
2023-06-29 17:41:01 +0300

    xs=12

--- src/components/InputAmount.jsx
+++ src/components/InputAmount.jsx
@@ -3,7 +3,7 @@ import { Grid, InputAdornment, TextField } from '@mui/material'
 
 function InputAmount() {
   return (
-    <Grid item>
+    <Grid item xs={12}>
       <TextField
       label="Amount"
       fullWidth
--- src/components/SelectCountry.jsx
+++ src/components/SelectCountry.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 
 function SelectCountry() {
   return (
-    <Grid item>
+    <Grid item xs={12}>
         
         <Autocomplete
+        value="option1"
        options={["option1", "option2"]}
        renderInput={(params) => <TextField {...params} label="from"/>}
       />
