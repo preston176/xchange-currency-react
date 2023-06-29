8ad56eeadab079031f61c6d2a67b2a5d755c2a76
preston176 <prestonnyamweya@gmail.com>
2023-06-29 23:49:51 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
-import { useState } from 'react'
-import { Button, Container, Grid, Typography } from '@mui/material'
-import InputAmount from './components/InputAmount'
+import { Box, Container, Grid, Link, Typography } from '@mui/material'
+import axios from 'axios'
+import { useContext, useEffect, useState } from 'react'
+import InputAmout from './components/InputAmout'
 import SelectCountry from './components/SelectCountry'
-import SwitchCurrency from './SwitchCurrency'
+import SwitchCurrency from './components/SwitchCurrency'
+import { CurrencyContext } from './context/CurrencyContext'
 
 function App() {
+  const {
+    fromCurrency,
+    setFromCurrency,
+    toCurrency,
+    setToCurrency,
+    firstAmount,
+  } = useContext(CurrencyContext);
+  const [resultCurrency, setResultCurrency] = useState(0);
+  const codeFromCurrency = fromCurrency.split(" ")[1];
+  const codeToCurrency = toCurrency.split(" ")[1];
+
+  useEffect(() => {
+    if(firstAmount) {
+      axios("https://api.freecurrencyapi.com/v1/latest", {
+        params: {
+          apikey: import.meta.env.VITE_API_KEY,
+          base_currency: codeFromCurrency,
+          currencies: codeToCurrency
+        }
+      })
+        .then(response => setResultCurrency(response.data.data[codeToCurrency]))
+        .catch(error => console.log(error))
+    }
+  }, [firstAmount, fromCurrency, toCurrency])
 
   const boxStyles = {
-    background: '#fdfdfd',
-    marginTop: "10rem",
+    background: "#fdfdfd",
+    marginTop: "10%",
     textAlign: "center",
     color: "#222",
     minHeight: "20rem",
@@ -19,18 +45,25 @@ function App() {
   }
 
   return (
-    <>
-      <Container maxWidth='md' sx={boxStyles}>
-      <Typography variant='h5' sx={{ marginBottom: '2rem'}}>Stay Ahead With Accurate Conversions</Typography>
+    <Container maxWidth="md" sx={boxStyles}>
+      <Typography variant='h5' sx={{ marginBottom: "2rem"}}>Stay Ahead with Accurate Conversions</Typography>
       <Grid container spacing={2}>
-        <InputAmount />
-        <SelectCountry />
+        <InputAmout />
+        <SelectCountry value={fromCurrency} setValue={setFromCurrency} label="From" />
         <SwitchCurrency />
-        <SelectCountry />
+        <SelectCountry value={toCurrency} setValue={setToCurrency} label="To" />
       </Grid>
-      </Container>
-       
-    </>
+
+      {firstAmount ? (
+        <Box sx={{ textAlign: "left", marginTop: "1rem"}}>
+          <Typography>{firstAmount} {fromCurrency} =</Typography>
+          <Typography variant='h5' sx={{ marginTop: "5px", fontWeight: "bold"}}>{resultCurrency*firstAmount} {toCurrency}</Typography>
+        </Box>
+      ) : ""}
+      <Typography fontSize="10px" sx={{ position: "absolute", bottom: "1rem", right: "1rem" }}>
+        <Link target="_blank" rel="noopener" href="https://twitter.com/Preston_176">Follow me on Twitter</Link>
+      </Typography>
+    </Container>
   )
 }
 
--- src/components/SelectCountry.jsx
+++ src/components/SelectCountry.jsx
@@ -1,14 +1,36 @@
-import { Autocomplete, Grid, TextField } from '@mui/material'
-import React from 'react'
+import { Autocomplete, Grid, Skeleton, TextField } from "@mui/material"
+import useAxios from "../hooks/useAxios"
 
-function SelectCountry() {
+const SelectCountry = (props) => {
+  const { value, setValue, label } = props;
+  const [data, loaded, error] = useAxios("https://restcountries.com/v3.1/all");
+
+  if(loaded) {
+    return (
+      <Grid item xs={12} md={3}>
+        <Skeleton variant="rounded" height={60}/>
+      </Grid>
+    )
+  }
+  if(error) {
+    return "Something went wrong!"
+  }
+
+  const dataFilter = data.filter(item => "currencies" in item);
+  const dataCountries = dataFilter.map(item => {
+    return `${item.flag} ${Object.keys(item.currencies)[0]} - ${item.name.common}`
+  });
+
   return (
     <Grid item xs={12} md={3}>
-        
-        <Autocomplete
-        value="option1"
-       options={["option1", "option2"]}
-       renderInput={(params) => <TextField {...params} label="from"/>}
+      <Autocomplete
+        value={value}
+        disableClearable
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        options={dataCountries}
+        renderInput={(params) => <TextField {...params} label={label} />}
       />
     </Grid>
   )
